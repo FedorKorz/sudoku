85f93d74ac6a82a5cf21c23609e1d8ab481767e4
FedorKorz <fedorkorz@.gmail.com>
2018-05-09 16:02:30 +0300

    all checks done

--- src/components/app.js
+++ src/components/app.js
@@ -32,6 +32,7 @@ class App extends Component {
   }
 
   findDuplicates(array) {
+    console.log('find duplicates: ' + array);
     let dup = [];
     array = array.sort();
     array.map((elem, i, a) => {
@@ -56,43 +57,65 @@ class App extends Component {
     return this.findDuplicates(transArr)
   }
 
-  checkBlocks(x, y) {
+  checkBlocks(x, y, item) {
     let temp = this.props.state.slice();
-    let flag = '';
+    let self = this;
 
     let storeOfCoord = {
-      0: ['00', '01', '02', '03', '04', '05','06', '07', '08'],
-      1: ['10', '11', '12', '13', '14', '15','16', '17', '18'],
-      2: ['20', '21', '22', '23', '24', '25','26', '27', '28'],
-      3: ['30', '31', '32', '33', '34', '35','36', '37', '38'],
-      4: ['40', '41', '42', '43', '44', '45','46', '47', '48'],
-      5: ['50', '51', '52', '53', '54', '55','56', '57', '58'],
-      6: ['60', '61', '62', '63', '64', '65','66', '67', '68'],
-      7: ['70', '71', '72', '73', '74', '75','76', '77', '78'],
-      8: ['80', '81', '82', '83', '84', '85','86', '87', '88']
+      0: ['00','01','02','10','11','12','20','21','22'],
+      1: ['03','04','05','13','14','15','23','24','25'],
+      2: ['06','07','08','16','17','18','26','27','28'],
+      3: ['30','31','32','40','41','42','50','51','52'],
+      4: ['33','34','35','43','44','45','53','54','55'],
+      5: ['36','37','38','46','47','48','56','57','58'],
+      6: ['60','61','62','70','71','72','80','81','82'],
+      7: ['63','64','65','73','74','75','83','84','85'],
+      8: ['66','67','68','76','77','78','86','87','88']
     }
 
     for (let key in storeOfCoord) {
       if (storeOfCoord[key].indexOf(x+''+y) !== -1) {
-        flag = key;
+        return detectRegion(key);
       }
     }
 
-    //0   0,1,2 slice 0,3
-    //1   0,1,2 slice 3,6
-    //2   0,1,2 slice 6,9
+    function sliceConcat(i, from, to) {
+      return self.findDuplicates(temp[i].slice(from,to).concat(temp[i+1].slice(from,to).concat(temp[i+2].slice(from,to))));
+    };
 
-    //3   3,4,5 slice 0,3
-    //4   3,4,5 slice 3,6
-    //5   3,4,5 slice 6,9
-
-    //6   6,7,8 slice 0,3
-    //7   6,7,8 slice 3,6
-    //8   6,7,8 slice 6,9
-
-    console.log(temp[x].slice(0,3).concat(temp[x+1].slice(0,3).concat(temp[x+2].slice(0,3))));
-
-    return
+    function detectRegion(flag) {
+      switch(flag) {
+       case '0':
+           return sliceConcat(0, 0, 3);
+           break;
+       case '1':
+           return sliceConcat(0, 3, 6);
+           break;
+       case '2':
+           return sliceConcat(0, 6, 9);
+           break;
+       case '3':
+           return sliceConcat(3, 0, 3);
+           break;
+       case '4':
+           return sliceConcat(3, 3, 6);
+           break;
+       case '5':
+           return sliceConcat(3, 6, 9);
+           break;
+       case '6':
+           return sliceConcat(6, 0, 3);
+           break;
+       case '7':
+           return sliceConcat(6, 3, 6);
+           break;
+       case '8':
+           return sliceConcat(6, 6, 9);
+           break;
+       default:
+           break;
+      }
+    };
   }
 
   render() {
@@ -104,20 +127,20 @@ class App extends Component {
               this.props.state.map((elem, i) => {
                 return <tr>
                 {elem.map((item, j) =>
-                  (item === ' ' || this.checkCollumn(j, i).indexOf(item) !== -1 || this.checkRow(i, j).indexOf(item) !== -1) ?
+                  (item === ' ' || this.checkBlocks(i, j).indexOf(item) !== -1 || this.checkRow(i, j).indexOf(item) !== -1 || this.checkCollumn(j, i).indexOf(item) !== -1) ?
                     <td
                       style={{color:'red'}}
                       onClick={ () => { this.testMeth(i,j)}}>
                         { this.props.state[i][j] }
                     </td> :
-                    <td> { item  } </td> )}
+                    <td> { item } </td> )}
                 </tr>})
             }
           </tbody>
         </table>
         { this.props.statePopup && <Popup /> }
         <button onClick={ this.onButtonGenerateSudoku }>Generate sudoku!</button>
-        <button onClick={ () => this.checkBlocks(0,0) }>Start</button>
+        <button>Start</button>
       </div>
     );
   }
